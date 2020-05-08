bf67878591b1d24c456b5dde6b2329c96681ef18
Jules Fouchy <jules.fouchy@ntymail.com>
2020-05-08 18:24:08 +0200

    Display authors by order of latesteRelease

--- src/components/AllAuthors.js
+++ src/components/AllAuthors.js
@@ -2,14 +2,16 @@ import { h } from 'hyperapp'
 import AuthorSection from './AuthorSection'
 
 export default (props) => h('div', {}, 
-    Object.values(props.authors).map( author => AuthorSection({
-        author: {
-            name: author.name,
-            link: author.link,
-        },
-        cards: author.cardIDs.map( id => ({
-            card: props.cards[id],
-            cardID: id,
+    Object.values(props.authors)
+        .sort( (a, b) => (new Date(b.dateLatestRelease)) - (new Date(a.dateLatestRelease)))
+        .map( author => AuthorSection({
+            author: {
+                name: author.name,
+                link: author.link,
+            },
+            cards: author.cardIDs.map( id => ({
+                card: props.cards[id],
+                cardID: id,
+            }))
         }))
-    }))
 )
