85ff8fdee17bc19b8e3705b94d776057cc77611d
Jules Fouchy <jules.fouchy@ntymail.com>
2020-05-08 18:27:14 +0200

    Show new cards first

--- src/components/authorSection.js
+++ src/components/authorSection.js
@@ -5,6 +5,8 @@ import Card from'./Card'
 export default props => {
     return h('div', {class: 'authorSection'}, [
         Author(props.author),
-        props.cards.map( card => Card(card) )
+        props.cards
+            .sort( (a, b) => (new Date(b.card.date)) - (new Date(a.card.date)))
+            .map( card => Card(card) )
     ])
 }
