6781325c233dee19af71ee0031cc5393f551a89a
Jules Fouchy <jules.fouchy@ntymail.com>
2020-05-23 13:19:24 +0200

    Fix : now works when their is no search for cardid

--- src/index.js
+++ src/index.js
@@ -12,9 +12,12 @@ import logger from "hyperapp-v2-basiclogger"
 import { ecFetchDatabase } from './actions/renderCards'
 
 const search = window.location.search
-const begin = search.indexOf('cardid=') + 'cardid='.length
+let begin = search.indexOf('cardid=')
+if (begin !== -1) begin += 'cardid='.length
 const end = search.indexOf('&')
-const cardID = end === - 1 ? search.substring(begin) : search.substring(begin, end)
+const cardID = begin === - 1 ? ''
+                : end === - 1 ? search.substring(begin)
+                  : search.substring(begin, end)
 
 app(
     { 
