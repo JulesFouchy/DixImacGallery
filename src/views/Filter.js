8dda02c3e9e7e3cf42575db69922074cff14d2c9
Jules Fouchy <jules.fouchy@ntymail.com>
2020-06-04 19:15:53 +0200

    Change url to reflect the filter you are currently applying

--- src/views/Filter.js
+++ src/views/Filter.js
@@ -1,25 +1,33 @@
 import { h } from 'hyperapp'
 import InputWithFilter from '../components/InputWithFilter'
 
+const createAction_GoToGalleryOfAuthor = (author) => (state) => {
+    window.history.pushState({}, null, '/?authorid='+author.id)
+    return {
+        ...state,
+        showOnlyThisAuthor: author.id,
+        authorFilter: author.name,
+        cardIDFilter: '',
+    }
+}
+
 const button_ResetFilter = () => h('button', 
     {
-        onclick: (state) => ({
-            ...state,
-            authorFilter: '',
-            cardIDFilter: '',
-            showOnlyThisAuthor: null,
-        })
+        onclick: (state) => {
+            window.history.pushState({}, null, '/')
+            return {
+                ...state,
+                authorFilter: '',
+                cardIDFilter: '',
+                showOnlyThisAuthor: null,
+            }
+        }
     }, 
     'See the whole gallery')
 
 const button_GoToGalleryOfAuthor = (author) => h('button', 
     {
-        onclick: (state) => ({
-            ...state,
-            showOnlyThisAuthor: author.id,
-            authorFilter: author.name,
-            cardIDFilter: '',
-        })
+        onclick: createAction_GoToGalleryOfAuthor(author)
     }, 
     'Other cards from this artist')
 
@@ -34,11 +42,7 @@ export default (props) => props.cardIDFilter === ''
             },
             authors: props.authors,
             bShowDropdown: props.isAuthorInputFocused,
-            onClick: (state, author) => ({
-                    ...state,
-                    showOnlyThisAuthor: author.id,
-                    authorFilter: author.name,
-            }),
+            onClick: (state, author) => createAction_GoToGalleryOfAuthor(author)(state),
         }),
         props.showOnlyThisAuthor !== null 
             && button_ResetFilter()
