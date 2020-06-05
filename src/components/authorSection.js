b6cf2d6f7b5bf3a1d2e21b9f4eb3857fc4d0e329
Jules Fouchy <jules.fouchy@ntymail.com>
2020-06-05 16:52:41 +0200

    Style : Work on alignment

--- src/components/authorSection.js
+++ src/components/authorSection.js
@@ -5,8 +5,10 @@ import Card from'./Card'
 export default props => {
     return h('div', {class: 'authorSection'}, [
         Author(props.author),
-        props.cards
-            .sort( (a, b) => (new Date(b.card.date)) - (new Date(a.card.date)))
-            .map( card => Card(card) )
+        h('div', {class: 'cardsList'}, 
+            props.cards
+                .sort( (a, b) => (new Date(b.card.date)) - (new Date(a.card.date)))
+                .map( card => Card(card) )
+        )
     ])
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,17 @@
 import { h } from 'hyperapp'
 import { ecRenderAndSetSrc } from '../actions/renderCards'
 
-export default (props) => h('span', {class: 'cardWrapper'}, 
-    h('img', 
-        {
-            class: 'card',
-            src: props.card.src
-        }
-    ),
-    props.card.generationMethod !== 0
-        && h('button', {onclick: state => [state, ecRenderAndSetSrc(props.card, props.cardID)]}, 'c')
+export default (props) => h('div', {class: 'cardWrapper'}, [
+        h('img',
+            {
+                class: 'card',
+                src: props.card.src
+            }
+        ),
+        props.card.generationMethod !== 0
+            && h('button', {
+                class: 'regenerateButton',
+                onclick: state => [state, ecRenderAndSetSrc(props.card, props.cardID)]
+            }, 'This card is random, press to see another version')
+    ]
 )
