be7d9691d5b4874390f8ec0ba7887a1773ef62ae
Jules Fouchy <jules.fouchy@ntymail.com>
2020-06-08 21:43:12 +0200

    Nice-ify the 'Play DixImac' link

--- src/views/header.js
+++ src/views/header.js
@@ -2,5 +2,5 @@ import { h } from 'hyperapp'
 
 export default () => h('header', {}, [
     h('h1', {}, 'DixImac Gallery'),
-    h('a', {href: 'http://diximac.herokuapp.com/'}, 'Play DixImac')
+    h('a', {href: 'http://diximac.herokuapp.com/'}, '▷ Play DixImac ◁')
 ])
