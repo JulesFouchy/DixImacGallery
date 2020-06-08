face7f92a96fcd2730afd76924309b6a54399e67
Jules Fouchy <jules.fouchy@ntymail.com>
2020-06-08 20:39:40 +0200

    Add a frame around author's names to better highlight them

--- src/components/author.js
+++ src/components/author.js
@@ -4,8 +4,8 @@ import LinkInsta from './LinkInsta'
 import LinkFB from './LinkFB'
 
 export default author => {
-    return h('div', {class: 'author'}, [
-        h('h2', {}, [
+    return h('div', {class: 'authorWrapper'}, [
+        h('h2', {class: 'author'}, [
             author.name,
             author.link && LinkPortfolio(author.link),
             author.linkInsta && LinkInsta(author.linkInsta),
